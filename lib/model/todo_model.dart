0f04fcc90d375f8d57cf0825f454dd85c03bef5b
Shazin Muhammed <122765461+ShazinCP@users.noreply.github.com>
2024-01-06 11:02:21 +0530

    adedd

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,13 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+
 part 'todo_model.freezed.dart';
 part 'todo_model.g.dart';
 
 @freezed
 class TodoModel with _$TodoModel {
   const factory TodoModel({
+    // ignore: invalid_annotation_target
     @JsonKey(name: "_id") String? id,
     String? title,
     String? description,
+    // ignore: invalid_annotation_target
     @JsonKey(name: "is_completed") bool? isCompleted,
   }) = _TodoModel;
 
